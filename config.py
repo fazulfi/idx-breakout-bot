2be89987c0845ce53e8ae605dd332d863d210957
faiz <fazulfim@gmail.com>
2026-02-26 13:56:52 +0000

    upedate

---
+++ config.py
@@ -0,0 +1,19 @@
+# ==============================
+# TELEGRAM CONFIG
+# ==============================
+
+TELEGRAM_TOKEN = "8275364554:AAG1BG3YUGOXVGVANygyW7pmftyqgXu5R-M"
+CHAT_ID = "1812698903"
+
+# ==============================
+# DATABASE
+# ==============================
+
+DB_PATH = "database/idx_data.db"
+
+# ==============================
+# SYSTEM
+# ==============================
+
+TIMEZONE = "Asia/Jakarta"
+TOP_N_SIGNAL = 10
